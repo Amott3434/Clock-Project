2e8b63733cac876e34d91381d0acdb336cb14d26
Amott3434 <amott@cpp.edu>
2026-04-06 16:53:10 -0700

    Stopwatch

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
-#include <string>
+#include <thread>
+#include <chrono>
 
-std::cout << "Hello, World!" << std::endl;
+int main() {
+    int seconds = 0;
+
+    while (true) {
+        int hrs = seconds / 3600;
+        int mins = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+
+        printf("%02d:%02d:%02d\n", hrs, mins, secs);
+
+        seconds++;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    return 0;
+}
